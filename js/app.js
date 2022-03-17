04855eb6a8d9c45c1c8e6897c19a15dea30f98d4
ardelgad <adelgadoporras@gmail.com>
2022-03-17 09:48:04 +0100

    clase

--- js/app.js
+++ js/app.js
@@ -1,22 +1,40 @@
 // Tu código aquí
 
-let form = document.forms["messages"];
-// let form = document.querySelector("form");
+//let form = document.forms["messages"];
+let form = document.querySelector("form");
 
 form.addEventListener('submit', function (e) {
-    console.log("Información sobre el objeto event: ", e)
-    e.preventDefault() // Curioso sobre lo que hace esto? Mira como se comporta el formulario cuando haces clic en "Enviar" si esta línea está comentada. 
+    //console.log("Información sobre el objeto event: ", e);
 
+    e.preventDefault(); // Curioso sobre lo que hace esto? Mira como se comporta el formulario cuando haces clic en "Enviar" si esta línea está comentada. 
+
+    //Información que le pongo
     let infoCaja = e.target.elements["message"].value;
-    // let infoCaja = document.forms["messages"].elements["message"].value; alternativa a la línea 10
-    // let infoCaja = document.querySelector(#message);
+    // let infoCaja = document.forms["messages"].elements["message"].value; //=> alternativas a la línea 12
+    // let infoCaja = document.querySelector("#message").value;
 
-    console.log("Hemos hecho clic en el botón de submit!", infoCaja)
+    //Comprobar que el texto que ha introducido el usuario no está vacío.
+    if (infoCaja == "") {
+        document.querySelector(".alert").style.display = "block";
+        //Como tenemos una clase preparada para enseñar la deberiamos usar
+        //document.querySelector(".feedback").classList.add("show");
+        setTimeout(hideMessage,2000);
+        function hideMessage() {
+            document.querySelector(".alert").style.display = "none";
+            console.log("han pasado 2 seg.");
+        }
+    }
 
-    document.querySelector(".message-content").textContent = infoCaja
-
-    if (infoCaja === "") {
-        document.querySelector(".message-content").textContent = "Ningún mensaje introducido"
+    else {
+        document.querySelector(".message-content").textContent = infoCaja;
     }
 
 });
+
+
+/* //Asincronia hacer aparecer y desaparecer cosas de la pagina web cada x tiempo
+setTimeout(myGreeting, 5000); //5000 milisegundos = 5 segundos
+console.log("acabo de cargar el script js");
+function myGreeting() {
+    console.log("Han pasado 5 segundos!");
+} */
